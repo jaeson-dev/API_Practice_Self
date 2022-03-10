124e0f15ed5fd82e4fdbb8115077d1cb40648088
jaeson-dev <jaeson.dev@gmail.com>
2022-03-10 22:37:23 +0900

    Initial Commit

---
+++ Api_Practice_self/Api_Practice_selfApp.swift
@@ -0,0 +1,17 @@
+//
+//  Api_Practice_selfApp.swift
+//  Api_Practice_self
+//
+//  Created by Jaeson.dev on 2022/03/10.
+//
+
+import SwiftUI
+
+@main
+struct Api_Practice_selfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
